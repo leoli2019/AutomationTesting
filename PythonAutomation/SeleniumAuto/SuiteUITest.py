3945a3e78be350ac29f8a2ab6b48dbfd1bbb92e8
Mikeli2016 <mikesiwer01@gmail.com>
2019-07-30 19:16:28 -0400

    Sample python

--- PythonAutomation/SeleniumAuto/SuiteUITest.py
+++ PythonAutomation/SeleniumAuto/SuiteUITest.py
@@ -1,10 +1,8 @@
 import unittest
-
 import PythonAutomation
 from PythonAutomation.SeleniumAuto.login_20_ui import TestLoginOrion
 from PythonAutomation.SeleniumAuto.NBAI_Pro_upload_ui import OrionProUploadUI
 from PythonAutomation.SeleniumAuto.HTML import HTMLStyle
-
 
 class MyTestSuite(unittest.TestCase):
 
@@ -15,7 +13,6 @@ class MyTestSuite(unittest.TestCase):
                 PythonAutomation.SeleniumAuto.NBAI_Pro_upload_ui.OrionProUploadUI),
             unittest.defaultTestLoader.loadTestsFromTestCase(PythonAutomation.SeleniumAuto.login_20_ui.TestLoginOrion),
         ])
-
         outfile = open("TestReport_UI/SeleniumReportUI.html", "w")
 
         runner1 = HTMLStyle(
@@ -23,9 +20,7 @@ class MyTestSuite(unittest.TestCase):
             title='UI Test Report',
             description='Selenium Tests'
         )
-
         runner1.run(smoke_test)
-
 
 if __name__ == '__main__':
     unittest.main()
